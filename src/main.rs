1086ac90f0143d3755ed734a52e4592dbce7e37c
saimonproyectsv2 <simon.patino@udea.edu.co>
2025-01-03 23:37:49 -0500

    second integration

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,23 @@ use std::io::{self, Read};
 use crossterm::terminal::enable_raw_mode;
 use crossterm::terminal::disable_raw_mode;
 
+
 fn main() {
-    enable_raw_mode().unwrap();
+    enable_raw_mode().unwrap(); // Enable raw mode
     for b in io::stdin().bytes() {
-        let c = b.unwrap() as char;
-        println!("{}", c);
+        let b = b.unwrap();
+        let c = b as char;
+        if c.is_control() {
+            println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
+        } else {
+            println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
+        }
         if c == 'q' {
-			disable_raw_mode().unwrap();
+            disable_raw_mode().unwrap(); // Disable raw mode
             break;
+
+            //raw mode is a mode that allows the program to read the input fron terminal by character instead of line 
+            
         }
     }
 }
